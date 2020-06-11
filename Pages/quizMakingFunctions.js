1c062ea12a230ca8aafabc33ca2686617fbd6cb0
glew2 <glewisonrv@gmail.com>
2020-06-10 22:30:13 -0400

    6/10 removeAnswer works with only spacing errors

--- Pages/quizMakingFunctions.js
+++ Pages/quizMakingFunctions.js
@@ -13,26 +13,26 @@ function addQuestion() {
     "<button type=\"button\" onclick=\"removeAnswer(" + questionCount + ")\">Remove Answer</button>" +
     "<button type=\"button\" onclick=\"removeQuestion(" + questionCount + ")\">Remove Question</button><br>" +
     "<div id=\"q" + questionCount + "answers\">" +
-        "<label for=\"a1\">A1:</label>\n" +
+        "<label for=\"a1\" id=\"q" + questionCount + "a1" + "label\">A1:</label>\n" +
         "<input type=\"text\" id=\"q" + questionCount + "a1\"><br>\n" +
-        "<label for=\"a2\">A2:</label>\n" +
+        "<label for=\"a2\" id=\"q" + questionCount + "a2" + "label\">A2:</label>\n" +
         "<input type=\"text\" id=\"q" + questionCount + "a2\"><br>\n" +
-        "<label for=\"a3\">A3:</label>\n" +
+        "<label for=\"a3\" id=\"q" + questionCount + "a3" + "label\">A3:</label>\n" +
         "<input type=\"text\" id=\"q" + questionCount + "a3\"><br>\n" +
-        "<label for=\"a4\">A4:</label>\n" +
+        "<label for=\"a4\" id=\"q" + questionCount + "a4" + "label\">A4:</label>\n" +
         "<input type=\"text\" id=\"q" + questionCount + "a4\"><br>\n" +
     "</div>" +
             
     "<label for=\"a\">Select The Correct Answer:</label>\n" +
     "<div id=\"q" + questionCount + "selections\">\n" +
         "<input name=\"q" + questionCount + "radioGroup\" type=\"radio\" id=\"qR" + questionCount + "a1\" value=\"a1\" checked>\n" +
-        "<label for=\"a1\">Question 1</label>\n" +
+        "<label for=\"a1\" id=\"qR" + questionCount + "a1" + "label\">Question 1</label>\n" +
         "<input name=\"q" + questionCount + "radioGroup\" type=\"radio\" id=\"qR" + questionCount + "a2\" value=\"a2\">\n" +
-        "<label for=\"a2\">Question 2</label>\n" +
+        "<label for=\"a2\" id=\"qR" + questionCount + "a2" + "label\">Question 2</label>\n" +
         "<input name=\"q" + questionCount + "radioGroup\" type=\"radio\" id=\"qR" + questionCount + "a3\" value=\"a3\">\n" +
-        "<label for=\"a3\">Question 3</label>\n" +
+        "<label for=\"a3\" id=\"qR" + questionCount + "a3" + "label\">Question 3</label>\n" +
         "<input name=\"q" + questionCount + "radioGroup\" type=\"radio\" id=\"qR" + questionCount + "a4\" value=\"a4\">\n" +
-        "<label for=\"a4\">Question 4</label>\n" +
+        "<label for=\"a4\" id=\"qR" + questionCount + "a4" + "label\">Question 4</label>\n" +
     "</div><br>" +
     "</div>";
     questionCount++;
@@ -44,6 +44,7 @@ function addAnswer(questionNum) {
     const div = document.getElementById("q" + questionNum + "answers");
     let label = document.createElement("LABEL");
     label.setAttribute("for", "a" + (answerCount + 1));
+    label.setAttribute("id", "q" + questionNum + "a" + (answerCount + 1) + "label");
     label.textContent = "A" + (answerCount + 1) + ":\n";
     let input = document.createElement("INPUT");
     input.setAttribute("type", "text");
@@ -61,19 +62,25 @@ function addAnswer(questionNum) {
     radioInput.setAttribute("value", "a" + (answerCount + 1));
     let radioLabel = document.createElement("LABEL");
     radioLabel.setAttribute("for", "a" + (answerCount + 1));
+    radioLabel.setAttribute("id", "qR" + questionNum + "a" + (answerCount + 1) + "label");
     radioLabel.textContent = "Question " + (answerCount + 1) + "\n";
     radioDiv.appendChild(radioInput); radioDiv.appendChild(radioLabel);
 }
             
 function removeAnswer(questionNum) {
     const answerCount = document.getElementsByName("q" + questionNum + "radioGroup").length;
+    
     const answerRadio = document.getElementById("qR" + questionNum + "a" + (answerCount));
+    const radioLabel = document.getElementById("qR" + questionNum + "a" + (answerCount) + "label");
     const answerInput = document.getElementById("q" + questionNum + "a" + (answerCount));
+    const inputLabel = document.getElementById("q" + questionNum + "a" + (answerCount) + "label");
 
-    answerRadio.remove(); // remove radiobutton
-    // ansDiv.removeChild(document.getElementById()); // remove radiobutton label
-    answerInput.remove(); // remove answer label
-    // radioDiv.removeChild(document.getElementById()); // remove answer input box
+    if (answerCount>2) {
+        answerRadio.remove(); // remove radiobutton
+        radioLabel.remove(); // remove radiobutton label
+        answerInput.remove(); // remove answer label
+        inputLabel.remove(); // radioDiv.removeChild(document.getElementById()); // remove answer input box
+    }
 }
 
 function removeQuestion(questionNum) {
